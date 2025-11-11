36e96d69ef8ee5dd57ded7cfd54f6706c979bd34
Nathan <chicanathan@gmail.com>
2025-11-10 18:58:52 -0800

    add tests for playersettingsview and shareboardview

--- frontend/src/components/game_info_panel/bottom_drawer_views/ShareBoardView.tsx
+++ frontend/src/components/game_info_panel/bottom_drawer_views/ShareBoardView.tsx
@@ -11,9 +11,7 @@ function ShareBoardView() {
 
     return (
         <div className="w-full flex flex-col gap-6 pb-8">
-            <h2 id="share-board-modal-title" className="text-lg text-slate-50 font-semibold">
-                Share Board
-            </h2>
+            <h2 className="text-lg text-slate-50 font-semibold">Share Board</h2>
 
             <CopyableTextField text={fenString} label="FEN" id="fen-string" copyButtonAriaLabel="Copy FEN" />
         </div>
